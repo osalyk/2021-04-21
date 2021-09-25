1727757eb6d82f6dd1230b556d422ac604295bf6
Jan M Michalski <jan.m.michalski@intel.com>
2021-09-25 20:35:49 +0100

    example R: add scripts and *.c files required for the story

---
+++ examples/R/run_test_ping.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ex
+#
+# XXX placeholder
+#
+
+SERVER_IP=192.168.1.100
+ping -c5 $SERVER_IP
---
+++ examples/R/simple_server.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+/*
+ * server.c -- a simple connection server XXX
+ */
+
+#include <librpma.h>
+
+#include "connection.h"
+
+int
+main(int argc, char *argv[])
+{
+	/* XXX */
+
+	return 0;
+}
